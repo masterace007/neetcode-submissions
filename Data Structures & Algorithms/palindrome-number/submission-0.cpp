337393e089b9351b102595e307063cd47ec971e5
smoking_ace <singh.shobhit007@gmail.com>
2026-08-27 11:03:25 +0530

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+        return 0;
+
+        string str = to_string(x);
+
+        for(int i = 0, j = str.length()-1; i < j; ++i, --j){
+            if(str[i] != str[j])
+            return 0;
+        }
+        return 1;
+    }
+};
