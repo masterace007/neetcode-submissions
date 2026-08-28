8b02c05d3c03adef91e7da82b7ba99fdd994d10a
smoking_ace <singh.shobhit007@gmail.com>
2026-08-28 12:42:11 +0530

    Add: string-matching-in-an-array - submission-0

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool static cmp(string a, string b){
+        return a.length() < b.length();
+    }
+    
+    vector<string> stringMatching(vector<string>& words) {
+        sort(begin(words),end(words),cmp);
+        vector<string> ans;
+        for(int i = 0; i < words.size(); ++i){
+            for(int j = i + 1; j < words.size(); ++j){
+                if(words[j].find(words[i]) != string::npos){
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
